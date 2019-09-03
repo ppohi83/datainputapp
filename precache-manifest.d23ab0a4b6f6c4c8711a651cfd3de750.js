2073953cf2cee3bbd5dd86f9ef4d8d59410dcbc8
leeinwoo <ppohi83@naver.com>
2019-09-03 13:30:24 +0900

    Updates

---
+++ precache-manifest.d23ab0a4b6f6c4c8711a651cfd3de750.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2668518c8cedef95c24186ef8544ae50",
+    "url": "/datainputapp/index.html"
+  },
+  {
+    "revision": "eb493800a94a1f8e777e",
+    "url": "/datainputapp/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "c4d2d927e18841549307",
+    "url": "/datainputapp/static/js/2.5fd5d7c0.chunk.js"
+  },
+  {
+    "revision": "eb493800a94a1f8e777e",
+    "url": "/datainputapp/static/js/main.bb0582a2.chunk.js"
+  },
+  {
+    "revision": "4114bce40b6f34b064c6",
+    "url": "/datainputapp/static/js/runtime~main.3084e378.js"
+  }
+]);
